4a325560b648040ab880454078ea855c1955431c
Abdelaziz <abdelazizMalfy@gmail.com>
2019-06-22 18:52:19 +0200

    Updates

---
+++ precache-manifest.08a2d6d6ead96e099dc73938a94a7891.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e87bf90e937022f102796f9c639306b4",
+    "url": "/AbdelazizMalfy/man-city.git/index.html"
+  },
+  {
+    "revision": "32f58deb7a5bf42a2a00",
+    "url": "/AbdelazizMalfy/man-city.git/static/css/main.3513261b.chunk.css"
+  },
+  {
+    "revision": "ca1af5588b33ccd87c8f",
+    "url": "/AbdelazizMalfy/man-city.git/static/js/2.10f78aa7.chunk.js"
+  },
+  {
+    "revision": "32f58deb7a5bf42a2a00",
+    "url": "/AbdelazizMalfy/man-city.git/static/js/main.1716781c.chunk.js"
+  },
+  {
+    "revision": "7ce4ffc5c507125e2fc5",
+    "url": "/AbdelazizMalfy/man-city.git/static/js/runtime~main.be8b804e.js"
+  },
+  {
+    "revision": "34e300e4fce09e33d958a059176f2b98",
+    "url": "/AbdelazizMalfy/man-city.git/static/media/Raheem.34e300e4.png"
+  },
+  {
+    "revision": "6de3706ade22bf1989b3853dda736986",
+    "url": "/AbdelazizMalfy/man-city.git/static/media/featured_player.6de3706a.png"
+  },
+  {
+    "revision": "dd26db2771a362bbd922fa4686b5218a",
+    "url": "/AbdelazizMalfy/man-city.git/static/media/jersey.dd26db27.jpg"
+  },
+  {
+    "revision": "1d5b64ec5f8fe8089fd9370edd244ac4",
+    "url": "/AbdelazizMalfy/man-city.git/static/media/manchester_city_logo.1d5b64ec.png"
+  }
+]);
